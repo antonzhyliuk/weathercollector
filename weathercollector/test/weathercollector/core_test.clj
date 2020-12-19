9d7efd2c687457cf3ca3137a7a7c7ec50eefde4a
Anton Žyluk <crashtown.pal@gmail.com>
2020-12-19 13:44:07 +0300

    Initial commit

---
+++ weathercollector/test/weathercollector/core_test.clj
@@ -0,0 +1,7 @@
+(ns weathercollector.core-test
+  (:require [clojure.test :refer :all]
+            [weathercollector.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
